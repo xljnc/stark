9635ffede19d67258fbfe83043da05b075fa205e
qiyu <jsj0804wt@126.com>
2020-01-03 11:55:51 +0800

    初次提交 by qiyu 2020-Jan-03

---
+++ src/main/java/com/wt/stark/util/CodeGenerator.java
@@ -0,0 +1,14 @@
+package com.wt.stark.util;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @author: qiyu
+ * @date: 2020/1/3 11:49
+ * @description:
+ */
+@Component
+public class CodeGenerator {
+
+
+}
