921e1c8dc7c5b093d1f214f8e5d7cb3637ac6810
qiyu <jsj0804wt@126.com>
2020-01-03 11:41:55 +0800

    first commit

---
+++ src/main/java/com/wt/stark/StarkApplication.java
@@ -0,0 +1,13 @@
+package com.wt.stark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StarkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wt/stark/StarkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wt.stark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
