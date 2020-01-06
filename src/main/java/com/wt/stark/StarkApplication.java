25b3804415bfef99757b11352f30c0ec2409c795
qiyu <jsj0804wt@126.com>
2020-01-06 11:00:14 +0800

    模版配置 by qiyu 2020-Jan-06

--- src/main/java/com/wt/stark/StarkApplication.java
+++ src/main/java/com/wt/stark/StarkApplication.java
@@ -2,8 +2,11 @@ package com.wt.stark;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@EnableConfigurationProperties
 public class StarkApplication {
 
     public static void main(String[] args) {
